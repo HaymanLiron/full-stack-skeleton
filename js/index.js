9e6fe7fcf93795a84b41f0c751e9f3e2372056f3
Liron <liron.hayman@gmail.com>
2016-08-04 23:08:05 +0300

    added initial files

---
+++ js/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by itc_user on 8/4/2016.
+ */
---
+++ main.py
@@ -0,0 +1,30 @@
+from bottle import route, run, template, static_file, request, response
+import json
+
+
+@route('/', method='GET')
+def index():
+    return template("index.html")
+
+
+@route('/js/<filename:re:.*\.js>', method='GET')
+def javascripts(filename):
+    return static_file(filename, root='js')
+
+
+@route('/css/<filename:re:.*\.css>', method='GET')
+def stylesheets(filename):
+    return static_file(filename, root='css')
+
+
+@route('/images/<filename:re:.*\.(jpg|png|gif|ico)>', method='GET')
+def images(filename):
+    return static_file(filename, root='images')
+
+
+def main():
+    run(host='localhost', port=7000)
+
+
+if __name__ == '__main__':
+    main()
